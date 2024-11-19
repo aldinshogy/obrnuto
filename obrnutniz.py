cfb6affe19d53bacb370e36c873660c74184bc17
aldinshogy <155404638+aldinshogy@users.noreply.github.com>
2024-11-20 00:02:36 +0100

    Update obrnutniz.py

--- obrnutniz.py
+++ obrnutniz.py
@@ -2,17 +2,12 @@ import re
 import streamlit as st
 
 def reverse_list():
-    # Unos korisnika
-    user_input = input("Unesite elemente liste odvojene zarezima (npr. 'Nema,Win 95,Win 98,Win 2000'): ")
+    user_input = input("Unesite elemente liste odvojene zarezima")
     
-    # Pretvaranje unosa u listu bez razmaka
     original_list = [element.strip() for element in user_input.split(',')]
     
-    # Obrnuti redoslijed liste
     reversed_list = original_list[::-1]
     
-    # Prikaz rezultata bez razmaka u izlazu
-    print("Obrnuti redoslijed elemenata liste je: " + ",".join(reversed_list))
+    print("Obrnuti redoslijed je: " + ",".join(reversed_list))
 
-# Poziv funkcije
 reverse_list()
